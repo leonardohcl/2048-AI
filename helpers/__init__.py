6366efcf6b2d0a56809ff97482e93910e28cf7d2
Leonardo Longo <leonardo.longo@valtech.com>
2023-02-11 18:38:39 -0300

    added parallel execution to random statistics

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -75,20 +75,24 @@ def get_variations(board: list[int], width: int, height: int):
     return horizontal_variation, vertical_variation
 
 
-def play_randomly(game: GameController, max_moves=0, print_play= False, play_title:str=None):
+def play_randomly(
+    game: GameController, max_moves=0, print_play=False, play_title: str = None
+):
     moves = 0
-    show_game = print_game if print_play else lambda game, name:None
+    show_game = print_game if print_play else lambda game, name: None
     can_move = lambda moves: moves <= max_moves if max_moves > 0 else lambda moves: True
     if game.is_winner() or game.is_game_over():
         game.start()
         show_game(game, play_title)
-        
+
     while can_move(moves) and not game.is_winner() and not game.is_game_over():
         game.move(game.get_random_move())
         moves += 1
         show_game(game, play_title)
-    
-    return moves
+
+    highest_block = game.board.highest_block()
+    return moves, game.score, highest_block
+
 
 def print_game(game: GameController, name: str = None):
     os.system("cls")
